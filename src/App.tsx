7308605f034c2942c8cf53e8baa37dc995e83524
BarriosUrzua <david@barriosurzua.com>
2023-06-10 20:18:17 -0500

    Added the Alert and Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import ListGroup from './components/ListGroup';
+import { useState } from 'react';
+import Alert from './components/Alert';
+import Button from './components/Button';
 
 function App() {
-  const cities = ['New York', 'San Francisco', 'London', 'Paris'];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-    document.title = `Selected: ${item}`;
-  };
+  const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <div>
-      <ListGroup
-        items={cities}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible ? (
+        <Alert isDismissable={true} onClose={() => setAlertVisible(false)}>
+          Click the X button to <strong>close</strong> this alert
+        </Alert>
+      ) : null}
+      <Button onClick={() => setAlertVisible(true)}>Click to show alert</Button>
     </div>
   );
 }
