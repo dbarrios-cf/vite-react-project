601a2a2d2965ddd16b63ea1071c0b2d38567958a
dbarrios-cf <dbarrios@centerfield.com>
2023-06-20 15:31:12 -0600

    Updating Arrays Apdated the App component to reflect the way of updating array data in react state as described in this lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 function App() {
-  const [isVisible, setVisibility] = useState(false);
+  const [fieldValue, setFieldValue] = useState('');
+  const [tags, setTags] = useState(['happy', 'cheerful']);
 
-  const handleClick = () => {
-    setVisibility(!isVisible);
-  };
+  let stringTags = ' ';
+
+  for (let index = 0; index < tags.length; index++) {
+    const tag = tags[index];
+
+    if(index + 1 === tags.length) {
+      stringTags += ` and ${tag}.`;
+    } else {
+      stringTags += ` ${tag},`;
+    }
+  }
+
+  if(tags.length === 0) {
+    stringTags = "n't."
+  }
+
+  const handleAddClick = () => {
+    setTags([...tags, fieldValue]);
+    setFieldValue('');
+  }
+
+  const handleRemoveClick = () => {
+    setTags(tags.filter((tag) => tag !== fieldValue));
+    setFieldValue('');
+  }
+
+  const handleChange = (e) => {
+    const {value} = e.target;
+    setFieldValue(value);
+  }
 
   return (
-    <>
-      <button onClick={handleClick}>Toggle Visibility</button>
-      {isVisible ? (
-        <div>
-          <h1>Yehaaaw!</h1>
-        </div>
-      ) : null}
-    </>
+    <div>
+      <h1>Hi our company is{stringTags}</h1>
+      <input type="text" name="attribute" id="attribute" value={fieldValue} onChange={handleChange}/>
+      <button onClick={handleAddClick}>Add element</button>
+      <button onClick={handleRemoveClick}>Remove element</button>
+    </div>
   );
 }
 
